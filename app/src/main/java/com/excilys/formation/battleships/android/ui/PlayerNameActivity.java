8988e111d7bd47a07c3a04f2e5f0670a52e8b164
leoa <leoa@theodo.fr>
2018-10-11 11:52:28 +0200

    step 1

--- app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
+++ app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
@@ -2,6 +2,7 @@ package com.excilys.formation.battleships.android.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import battleships.formation.excilys.com.battleships.R;
 
@@ -11,5 +12,8 @@ public class PlayerNameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_name);
+    }
+
+    public void onClickButton(View view) {
     }
 }
