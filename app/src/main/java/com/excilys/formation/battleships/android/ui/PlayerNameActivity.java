80fcc90ac418b87ffea7d3bdbbcb7c0048f34460
leoa <leoa@theodo.fr>
2018-10-12 12:46:13 +0200

    bonus 2

--- app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
+++ app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
@@ -1,5 +1,6 @@
 package com.excilys.formation.battleships.android.ui;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,16 +9,25 @@ import android.widget.EditText;
 import battleships.formation.excilys.com.battleships.R;
 
 public class PlayerNameActivity extends AppCompatActivity {
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_name);
+        preferences = getApplicationContext().getSharedPreferences("Pref", MODE_PRIVATE);
+
+        String playerName = preferences.getString("playerName", null);
+
+        if(playerName != null) {
+            BattleShipsApplication.getGame().init(playerName);
+        }
     }
 
     public void onClickButton(View view) {
         EditText editText = (EditText) findViewById(R.id.text_name);
         String playerName =  editText.getText().toString();
+        preferences.edit().putString("playerName", playerName).apply();
 
         BattleShipsApplication.getGame().init(playerName);
     }
